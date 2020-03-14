35871fb1d1113441482a0c9076dd4bc17e82306e
Somin Park <48925632+sominparrrk@users.noreply.github.com>
2020-03-14 09:48:07 +0900

    Added examples from oss project

---
+++ addedExamples/follow_followers_by_containedKeywords.py
@@ -0,0 +1,28 @@
+"""
+    instabot example
+    Workflow:
+        Follow user's followers by username by contained keywords
+"""
+
+import argparse
+import os
+import sys
+
+sys.path.append(os.path.join(sys.path[0], "../"))
+from instabot import Bot  # noqa: E402
+
+parser = argparse.ArgumentParser(add_help=True)
+parser.add_argument("-u", type=str, help="username")
+parser.add_argument("-p", type=str, help="password")
+parser.add_argument("-proxy", type=str, help="proxy")
+parser.add_argument("users", type=str, nargs="+", help="users")
+args = parser.parse_args()
+
+bot = Bot()
+bot.login(username=args.u, password=args.p, proxy=args.proxy)
+
+key = input("Input contained keywords : ")
+
+for username in args.users:
+    #bot.follow_followers(username)
+    bot.follow_contain_str_follower(username,key)

0e0837e8191e101c9cc1c2269c57e5d1c3f65b6f
Somin Park <48925632+sominparrrk@users.noreply.github.com>
2020-03-14 09:35:20 +0900

    Update bot_photo.py

--- instabot/bot/bot_photo.py
+++ instabot/bot/bot_photo.py
@@ -2,10 +2,15 @@ import os
 from io import open
 
 from tqdm import tqdm
-
+import cv2
 
 def upload_photo(
-    self, photo, caption=None, upload_id=None, from_video=False, options={}
+    self,
+    photo,
+    caption=None,
+    upload_id=None,
+    from_video=False,
+    options={}
 ):
     """Upload photo to Instagram
 
@@ -24,7 +29,11 @@ def upload_photo(
     """
     self.small_delay()
     result = self.api.upload_photo(
-        photo, caption, upload_id, from_video, options=options
+        photo,
+        caption,
+        upload_id,
+        from_video,
+        options=options
     )
     if not result:
         self.logger.info("Photo '{}' is not uploaded.".format(photo))
@@ -34,7 +43,11 @@ def upload_photo(
 
 
 def download_photo(
-    self, media_id, folder="photos", filename=None, save_description=False
+    self,
+    media_id,
+    folder="photos",
+    filename=None,
+    save_description=False
 ):
     self.small_delay()
 
@@ -66,7 +79,53 @@ def download_photos(self, medias, folder, save_description=False):
     self.logger.info("Going to download {} medias.".format(len(medias)))
 
     for media in tqdm(medias):
-        if not self.download_photo(media, folder, save_description=save_description):
+        if not self.download_photo(
+            media, folder, save_description=save_description
+        ):
             self.error_delay()
-            broken_items = medias[medias.index(media) :]
+            broken_items = medias[medias.index(media):]
     return broken_items
+
+
+#추가한 코드
+# 매개변수로 미디어들의 리스트, 유저이름, 저장된 폴더 위치를 받는다.
+def check_downloaded_photos(self, medias, username, folder):
+    # 방금 저장한 medias인지 확인하기 위한 변수
+    downloaded = []
+    # 삭제한 사진 갯수를 저장하기 위한 변수
+    deletecount = 0
+    # 매개변수로 받은 folder 내에 있는 모든 파일들의 리스트
+    photolist = os.listdir(folder)
+    
+    # medias 리스트를 반복문으로 돌면서
+    for i in range(len(medias)):
+
+        # 사진을 다운로드 할 때 저장하는 이름
+        t = username[1:] + "_" + medias[i]
+        tjpg = t + ".jpg"
+
+        print(t)
+
+        # 사진의 이름이 photolist 리스트에 있다면 downloaded 리스트 끝에 추가
+        if tjpg in photolist:
+            downloaded.append(tjpg)
+        else:
+            # 한 media에 사진이 여러개라면 사진 이름이 다운로드한 사진 뒤에 순번이 붙기 때문에
+            # photolist 안에 사진의 이름이 있나 확인하면 안되고
+            # photolist 의 요소가 사진 이름 안에 포함되는지 확인해야 한다.
+            # photolist 안에 있는 요소안에 사진 이름이 있다면 downloaded 리스트 끝에 추가한다.
+            for j in photolist:
+                if t in j:
+                    downloaded.append(j)
+    # 다운로드한 사진들을 반복문으로 돌면서
+    for x in range(len(downloaded)):
+        # downloaded 리스트에 있는 사진을 창에 띄운다.
+        img = cv2.imread(folder + "\\" + downloaded[x], cv2.IMREAD_COLOR)
+        cv2.imshow('image', img)
+        # 눌린 키가 esc라면 해당 사진을 삭제한다.
+        if cv2.waitKey(0) == 27:
+            cv2.destroyAllWindows()
+            os.remove(folder + "\\" + downloaded[x])
+            deletecount += 1
+    # 삭제한 사진의 갯수 출력한다.
+    self.logger.info("Deleted {} photos.".format(deletecount))
